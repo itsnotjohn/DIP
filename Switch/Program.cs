2efb936441c9b06ce9cf6e53fba3666270e77b68
João Rodrigues <42720127+itsnotjohn@users.noreply.github.com>
2025-06-12 20:31:18 -0300

    DIP (Legado)
    Legado

---
+++ Switch/Program.cs
@@ -0,0 +1,24 @@
+﻿enum State { On, Off }
+
+class Lamp(State State)
+{
+    public void Operate()
+    {
+        State = State == State.On ? State.Off : State.On;
+        Console.WriteLine("Luz " + (State == State.On ? "Ligada" : "Deslgiada"));
+    }
+}
+
+class Switch
+{
+    private Lamp lamp;
+    public Switch(Lamp device)
+    {
+        lamp = device;
+    }
+
+    public void Press()
+    {
+        lamp.Operate();
+    }
+}
